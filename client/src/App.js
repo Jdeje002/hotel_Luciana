5879cd447ecefdbd8ea612a090b28b2c1da40f69
John De jesus <John@Johns-iMac.local>
2018-08-02 17:01:24 -0700

    added spacing

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./Components/Navbar"
 import Main from "./assets/main.jpg"
 import Jumbotron from "./Components/Jumbotron"
+
 class App extends Component {
   render() {
     return (
