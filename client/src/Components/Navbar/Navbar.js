90475ffe60016f0124e53d387ab1b6f491edb3f7
John De jesus <John@Johns-iMac.local>
2018-08-02 17:33:43 -0700

    create more

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -5,43 +5,43 @@ import Logo from "../../assets/Logo.jpg"
 
 class Navbar extends Component {
 
-  render() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-none">
-        <div className ="container-fluid">
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Photos <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Suits</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">About Us</a>
-              </li>
-            </ul>
-            <a className="navbar-brand" href="#"><img src={Logo} alt=""/></a>
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Dinning menu <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Direction</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Contact us</a>
-              </li>
-            </ul>
-          </div>
-          </div>
-        </nav>
-       
-    )
-  }
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-none">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Dinning Menu <span className="sr-only">(current)</span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Things To Do</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Rooms</a>
+                            </li>
+                        </ul>
+                        <a className="navbar-brand" href="#"><img src={Logo} alt="" /></a>
+                        <ul className="navbar-nav">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">About Us <span className="sr-only">(current)</span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Direction</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contact us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+        )
+    }
 
 }
 export default Navbar;
